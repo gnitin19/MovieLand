db030f1a52e3b1d08d9b428fbd8927f9d43cb6a3
NITIN <galla.nitin2003@gmail.com>
2023-03-07 17:49:59 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+//1dda94d6
+import App from './App';
+
+
+import { createRoot } from 'react-dom/client';
+
+const rootElement = document.getElementById('root');
+
+createRoot(rootElement).render(<App />);
